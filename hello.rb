029fec27d2bc2b37cdbf216d875724f167c96341
Raj Muhar <rajmuhar12@gmail.com>
2018-11-27 01:40:47 +0000

    more notes

--- hello.rb
+++ hello.rb
@@ -1,2 +1,12 @@
 puts "hello world!"
 
+gift = [
+          "half of a leftover burrito",
+          "a day-old cup of coffee",
+          "a tesla",
+          "a life-size quadrocopter",
+          "a slice of stale wedding cake",
+          "a medium-sized island off the coast of Sumatra",
+          "a visual prosthesis",
+         ]
+  puts "All I got for valentine's day was #{gift.sample}."
